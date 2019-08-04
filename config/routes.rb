1bd7ed0c1cb12d57a96caf3a25ff27b0b0bb73e0
syukihiro <kaui.yukihiro@gmail.com>
2019-08-04 22:38:44 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
-  devise_for :users
-  get '/wines/index'
-  
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  root 'wines#index'
+  get 'wines/index'
+
 end
