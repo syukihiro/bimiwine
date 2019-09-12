7b9c27cbd9eb60f0eed2d4a3e87f11ea6e340e2f
syukihiro <kaui.yukihiro@gmail.com>
2019-09-12 14:59:51 +0900

    Update wines_controller.rb

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -3,4 +3,21 @@ class WinesController < ApplicationController
   def index
   end
 
+  def new
+    @wine = Wine.new
+    @feature = Feature.all
+    @wine.wines_images.build
+  end
+
+  def create
+    @wine = Wine.new(wine_params)
+    @wine.save!
+  end
+
+  private
+
+  def wine_params
+    params.require(:wine).permit( :name, :comment, :shop_name, feature_ids: [], wines_images_attributes: [:images]).merge( user_id: current_user.id)
+  end
+
 end
