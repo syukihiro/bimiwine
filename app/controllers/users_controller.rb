7d39eb503333b5d64671f55e2b67030dded420b7
syukihiro <kaui.yukihiro@gmail.com>
2019-09-16 22:52:30 +0900

    controller.update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @wine = @user.wines
+    @winesimages = WinesImage.where(params[:id]).order("created_at DESC")
   end
 end
