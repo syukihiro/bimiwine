109c32eef04f109b456741a180c30edebb4336e4
syukihiro <kaui.yukihiro@gmail.com>
2019-09-20 14:01:38 +0900

    Update wines_controller.rb

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -9,17 +9,23 @@ class WinesController < ApplicationController
     @wine.wines_images.build
   end
 
-  def create
-    @wine = Wine.new(wine_params)
-    if @wine.save!
-      redirect_to postcomplete_wines_path
-    else
-      render 'new'
+    def create
+      @wine = Wine.new(wine_params)
+      if @wine.save!
+        redirect_to postcomplete_wines_path
+      else
+        render 'new'
+    end
   end
-end
 
   def postcomplete
 
+  end
+
+  def show
+    @wine = Wine.find(params[:id])
+    @wineimages = WinesImage.find(params[:id])
+    @feature = Feature.where(id: @wine.feature_ids)
   end
 
   private
