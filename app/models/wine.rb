170b8f3661bd0ea0dcf3fc588ec850f359efaffd
syukihiro <kaui.yukihiro@gmail.com>
2019-10-03 22:15:32 +0900

    Update wine.rb

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,6 +1,6 @@
 class Wine < ApplicationRecord
-  has_many :wines_images
-  has_many :wines_features_tags
+  has_many :wines_images, dependent: :destroy
+  has_many :wines_features_tags, dependent: :destroy
   has_many :features, through: :wines_features_tags
   accepts_nested_attributes_for :wines_images, update_only: true
   accepts_nested_attributes_for :wines_features_tags, update_only: true
