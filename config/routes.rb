5505d1021fc6401de90a3d2e09c73f128ca71662
syukihiro <kaui.yukihiro@gmail.com>
2019-10-03 22:15:19 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,13 @@ Rails.application.routes.draw do
   get 'wines/index'
   get 'users/:id' => 'users#show'
 
-  resources :wines, only: [:new, :create, :show, :edit, :update] do
+  resources :wines, only: [:new, :create, :show, :edit, :update, :destroy] do
     collection do
       get :wines
       get :postcomplete
       get :editcomplete
+      get :deletecomplete
     end
+      get :deletebefore, on: :member
   end
 end
