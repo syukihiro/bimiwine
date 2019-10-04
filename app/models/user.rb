5117b1202c1f54185c51eb5bd3f5dc5e1edd819c
syukihiro <kaui.yukihiro@gmail.com>
2019-10-04 15:20:30 +0900

    update-model-to-likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          has_many :wines
-  validates :nickname, presence: true
-  validates :phone_number, presence: true
+         has_many :likes, dependent: :destroy
+         has_many :liked_wines, through: :likes, source: :post
+         validates :nickname, presence: true
+         validates :phone_number, presence: true
+  def already_liked?(wine)
+    self.likes.exists?(wine_id: wine.id)
+  end
 end
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -2,6 +2,8 @@ class Wine < ApplicationRecord
   has_many :wines_images, dependent: :destroy
   has_many :wines_features_tags, dependent: :destroy
   has_many :features, through: :wines_features_tags
+  has_many :likes
+  has_many :liked_users, through: :likes, source: :user
   accepts_nested_attributes_for :wines_images, update_only: true
   accepts_nested_attributes_for :wines_features_tags, update_only: true
 end
