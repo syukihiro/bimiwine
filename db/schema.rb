de1f82ee477adc443ec0b65467219040013c0f1b
syukihiro <kaui.yukihiro@gmail.com>
2020-06-06 12:30:27 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2019_10_04_022114) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
-    t.string "phone_number", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
